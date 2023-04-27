cb01e15153fa8f5d513a5368a201cbad142fdfc2
Sergtran <sergiiosolano@hotmail.com>
2023-04-27 15:53:59 -0500

    Matriz y Spawner
    Se crea clase para construir las matrices y el instanciar las cartas

---
+++ Assets/Scripts/CardSpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSpawner : MonoBehaviour
+{
+    //aqui se necesita una variable de tipo lista que almace los prefabs de las cartas q tenemos
+
+    public void CreateMatrix(int size)
+    {
+        //metodo para crear matriz
+        SpawnCardsInMatrix();
+    }
+
+    void SpawnCardsInMatrix()
+    {
+        //metodo para spawnar las cartas en las posciones d ela matriz
+    }
+
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int cardsToSpawn = 0;
+    public CardSpawner cardSpawner;
+
+
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    private void Awake()
+    {
+        if (instance != this)
+        {
+            if (instance != null)
+            {
+                DestroyImmediate(instance.gameObject);
+            }
+        }
+        instance = this;
+
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SendCardsToSpawn(int amount)
+    {
+        cardSpawner.CreateMatrix(amount);
+    }
+}
