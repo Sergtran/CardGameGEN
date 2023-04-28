5142adf490ef3125b296c843bd680cb169110631
Santpa <64154990+spc1403@users.noreply.github.com>
2023-04-27 21:01:32 -0500

    Control de vidas e imagenes de vida restante

--- Assets/Scripts/LifeController.cs
+++ Assets/Scripts/LifeController.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class LifeController : MonoBehaviour
 {
-    public int inicialLife;
+    public int inicialLife = 5;
     public int currentLife;
+    public Image lifeImage;
+    public Sprite[] lifeSprites;
+
     void Start()
     {
-        
+        currentLife = inicialLife;   
+        lifeImage.sprite = lifeSprites[currentLife - 1];   
+
     }
 
     // Update is called once per frame
@@ -19,11 +24,21 @@ public class LifeController : MonoBehaviour
 
     public void DecreaseLife()
     {
-        //codigo con logica para perde vida
-
-        if (currentLife==0)
+        
+    bool isMatch = CompareCards(); // Llamamos a la función que devuelve un bool
+    if (!isMatch) // Si el resultado es falso
+    {
+        currentLife--; // Restamos una vida
+    
+        if (currentLife == 0)
         {
             GameManager.Instance.GameOver();
         }
+        else 
+        {
+            lifeImage.sprite = lifeSprites[currentLife - 1]
+        }
     }
 }
+
+
